15e469cfe95b858da0e74ca69430560a7de24438
randomuser <luoxiao1106@gmail.com>
2021-01-24 14:13:49 -0800

    factored error msg

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import {ReminderBook} from "./reminderBook";
+import { ReminderBook } from "./reminderBook";
 import { question } from "readline-sync";
 
 const menu = `
@@ -13,102 +13,106 @@ const menu = `
 |  [6] Exit 👋                
 ------------------------------
 `;
+let choice2: number = 0;
+let term: string = "";
 
-let choice: number = 0;
 let rBook = new ReminderBook();
-
 
 // let input:string = question("Hit [Enter] key to see main menu:");
 // if (input==="") {
-//     console.log(menu);   
+//     console.log(menu);
 // }
 
-function getInput(inputType:string) {
-    let searchTerm:string = "";
-    let confirm:string = "";
+function getInput(inputType: string) {
+  let searchTerm: string = "";
+  let confirm: string = "";
 
-    while (confirm != "y") {
-        searchTerm = question(`Enter a ${inputType} here: `);
-        confirm = question(`You entered ${inputType}: '${searchTerm}', is it correct? y/n: `);
-        if (confirm === "n"){
-            console.log('Please try typing it again');
-        } 
-        else if (confirm != "y") {
-            console.log('Invalid input: Please enter either y/n');
-        }
+  while (confirm != "y") {
+    searchTerm = question(`Enter a ${inputType} here: `);
+    confirm = question(
+      `You entered ${inputType}: '${searchTerm}', is it correct? y/n: `
+    );
+    if (confirm === "n") {
+      console.log("Please try typing it again");
+    } else if (confirm != "y") {
+      console.log("Invalid input: Please enter either y/n");
     }
-    return searchTerm;
+  }
+  return searchTerm;
 }
 
-while (choice != 6) {
-    let input1:string = question("Hit [Enter] key to see main menu:");
-    if (input1==="") {
-        console.log(menu);   
+while (choice2 != 6) {
+  let choice1: string = "none";
+  let input: string = "";
+  choice2 = 0;
+
+  while (choice1 != "") {
+    choice1 = question("Hit [Enter] key to see main menu:");
+    if (choice1 != "") {
+      console.log("Invalid entry, press Enter to see menu");
+      continue;
     }
-    else {
-        throw new Error('Invalid entry, press Enter to see menu')
+    console.log(menu);
+  }
+
+  while (
+    typeof parseInt(input) != "number" ||
+    ![1, 2, 3, 4, 5, 6].includes(parseInt(input))
+  ) {
+    input = question("Choose a [Number] followed by [Enter]: ");
+    if (
+      typeof parseInt(input) != "number" ||
+      ![1, 2, 3, 4, 5, 6].includes(parseInt(input))
+    ) {
+      console.log("Invalid entry, press Enter number 1-6.");
+    } else {
+      choice2 = parseInt(input);
+      switch (choice2) {
+        case 1: // Show all reminders
+          rBook.listNotes();
+          break;
+        case 2: // Search reminders
+          term = getInput("keyword");
+
+          rBook.searchNote(term);
+          break;
+        case 3: // Add reminder
+          let reminderBody: string = "";
+          let reminderTag: string = "";
+
+          reminderBody = getInput("reminder");
+          reminderTag = getInput("tag");
+
+          rBook.addNote(reminderBody, reminderTag);
+          console.log("Reminder added");
+
+          break;
+
+        case 4: // Modify reminders
+          rBook.displayList();
+          let editChoice = parseInt(getInput("reminder number to edit"));
+          let newDescription = getInput("Description");
+          let statusToggle: string = "";
+          rBook.editReminder(editChoice, newDescription);
+          while (statusToggle != "y" && statusToggle != "n") {
+            statusToggle = question(
+              `Do you wish to toggle the completed status? y/n: `
+            );
+          }
+          if (statusToggle === "y") {
+            rBook.toggleComplete(editChoice);
+          }
+          break;
+
+        case 5: // Toggle completion
+          rBook.displayList();
+          let toggleChoice = getInput("reminder number to toggle");
+          rBook.toggleComplete(parseInt(toggleChoice));
+          break;
+
+        default:
+          break;
+      }
     }
-
-    let input2:string = question("Choose a [Number] followed by [Enter]: ");
-    if (typeof input2 != "number" || [1,2,3,4,5,6].includes(input2)) {
-        console.log(menu);   
-    }
-    else {
-        throw new Error('Invalid entry, press Enter to see menu')
-    }
-    choice = parseInt(input2);
-
-    // if (input==="") { // redisplay menu when hit enter
-    //     console.log(menu);   
-    // }
-    if ([1,2,3,4,5].includes(choice)) {
-        let term: string = "";
-
-        switch (choice) {
-            case 1: 
-                rBook.listNotes();
-                break;
-            case 2: 
-                term = getInput('keyword');
-
-                rBook.searchNote(term);
-                break;
-            case 3:
-                let reminderBody:string = "";
-                let reminderTag:string = "";
-
-                reminderBody = getInput('reminder');
-                reminderTag = getInput('tag');
-
-                rBook.addNote(reminderBody,reminderTag);
-                console.log('Reminder added');
-                
-                break;
-
-            case 4:
-                rBook.displayList();
-                let editChoice = parseInt(getInput('reminder number to edit'));
-                let newDescription = getInput('Description');
-                let statusToggle: string = "";
-                rBook.editReminder(editChoice,newDescription);
-                while (statusToggle != "y" && statusToggle != "n" ) {
-                    statusToggle = question(`Do you wish to toggle the completed status? y/n: `);
-                }
-                if (statusToggle === "y"){
-                    rBook.toggleComplete(editChoice);
-                }
-                break;
-
-            case 5: 
-                rBook.displayList();
-                let toggleChoice = getInput('reminder number to toggle');
-                rBook.toggleComplete(parseInt(toggleChoice));
-                break;
-
-            default:
-                break;
-        }
-    }
+  }
 }
-
-
--- src/reminderBook.ts
+++ src/reminderBook.ts
@@ -1,154 +1,150 @@
 import { dir } from "console";
-import {Reminder} from "./reminder";
+import { Reminder } from "./reminder";
 
 interface ItemObj {
-    "body" : string,
-    "done" : boolean,
-    "r_id" : number
+  body: string;
+  done: boolean;
+  r_id: number;
 }
 
 class ReminderBook {
-    noteList: Reminder[] = [];
-        // [
-        //     Reminder { body: 'r1', tag: 'T1', done: false, r_id: 1 },
-        //     Reminder { body: 'r2', tag: 'T1', done: false, r_id: 2 },
-        //     Reminder { body: 'r3', tag: 'T2', done: false, r_id: 3 },
-        //     Reminder { body: 'r4', tag: 'R1', done: false, r_id: 4 }
-        // ]
+  noteList: Reminder[] = [];
+  // [
+  //     Reminder { body: 'r1', tag: 'T1', done: false, r_id: 1 },
+  //     Reminder { body: 'r2', tag: 'T1', done: false, r_id: 2 },
+  //     Reminder { body: 'r3', tag: 'T2', done: false, r_id: 3 },
+  //     Reminder { body: 'r4', tag: 'R1', done: false, r_id: 4 }
+  // ]
 
-    itemList: ItemObj[] = [];
+  itemList: ItemObj[] = [];
 
-    // constructor() {
-    //     this.addNote('buy large milk','grocery');
-    //     this.addNote('buy oranges','grocery');
-    //     this.addNote('learn typescript','coding');
-    // }
+  // constructor() {
+  //     this.addNote('buy large milk','grocery');
+  //     this.addNote('buy oranges','grocery');
+  //     this.addNote('learn typescript','coding');
+  // }
 
-    private _listObj()  {
-        let listObj: { [key: string]: ItemObj[] } = {};
+  private _listObj() {
+    let listObj: { [key: string]: ItemObj[] } = {};
 
-        this.noteList.forEach(ele => {
-            let tag: string = ele.tag;
-            let itemObj: ItemObj = {"body": ele.body, "done": ele.done, "r_id": ele.r_id}
+    this.noteList.forEach((ele) => {
+      let tag: string = ele.tag;
+      let itemObj: ItemObj = { body: ele.body, done: ele.done, r_id: ele.r_id };
 
-            if(!listObj[tag]){ // If a new tag, start a new list
-                listObj[tag] = [];
-                listObj[tag].push(itemObj);
-            }
-            else { // otherwise push to existing
-                listObj[tag].push(itemObj);
-            }
+      if (!listObj[tag]) {
+        // If a new tag, start a new list
+        listObj[tag] = [];
+        listObj[tag].push(itemObj);
+      } else {
+        // otherwise push to existing
+        listObj[tag].push(itemObj);
+      }
+    });
+    return listObj;
+  }
+
+  // case 1
+  listNotes() {
+    if (this.noteList.length == 0) {
+      console.log("\n You have no reminders \n");
+    } else {
+      Object.entries(this._listObj()).forEach(([tag, itemObj]) => {
+        console.log(`\n[Tag]: ${tag} \n`);
+        itemObj.forEach((item) => {
+          if (!item.done) {
+            console.log(`⭕️ ${item.body}`);
+          } else {
+            console.log(`🔴 ${item.body} [complete]`);
+          }
         });
-        return listObj;
+      });
+      console.log("");
     }
+  }
 
-    // case 1
-    listNotes(){
-        if (this.noteList.length == 0) {
-            console.log('\n You have no reminders \n');
-        }
-        else {
-            Object.entries(this._listObj()).forEach(
-                ([tag, itemObj]) => {
-                    console.log(`\n[Tag]: ${tag} \n`)
-                    itemObj.forEach((item)=> {
-                        if(!item.done) {
-                            console.log(`⭕️ ${item.body}`);
-                        }
-                        else {
-                            console.log(`🔴 ${item.body} [complete]`);
-                        }
-                    })
-                });
-                console.log('');
-        }
+  // case 2
+  searchNote(term: string) {
+    let listObj = this._listObj();
+    let tagFound = false;
 
-    }
-
-    // case 2
-    searchNote(term: string) {
-        
-        let listObj = this._listObj();
-        let tagFound = false;
-        
-         // search exact matching tags
-        Object.entries(listObj).forEach(
-            ([tag, itemObj]) => {                
-                if (tag === term.toUpperCase()) {
-                    tagFound = true;
-                    itemObj.forEach((item)=> {
-                        if(!item.done) {
-                            console.log(`⭕️ ${item.body}`);
-                        }
-                        else {
-                            console.log(`🔴 ${item.body} [complete]`);
-                        }
-                    })
-                }
-            });
-    
-
-        if (!tagFound) { // if no tag found above, proceed to search body value
-            Object.values(listObj).forEach( itemObj => { // looping tag level
-
-                itemObj.forEach( item => { // looping item-obj array
-                    if (item.body.toUpperCase() === term.toUpperCase() || item.body.toUpperCase().includes(term.toUpperCase()) ) {
-                        if(!item.done) {
-                            console.log(`⭕️ ${item.body}\n`);
-                        }
-                        else {
-                            console.log(`🔴 ${item.body} [complete]\n`);
-                        } 
-                    }
-                })
-            });
-        }
-    }
-
-    // case 3
-    addNote(body: string, tag: string) {
-        this.noteList.push(new Reminder(body, tag))
-    }
-
-    // display edit/toggle list
-    displayList(){
-        let counter: number = 1;
-        this.noteList.forEach(ele => {
-            let itemObj: ItemObj = {"body": ele.body, "done": ele.done, "r_id": ele.r_id}
-            this.itemList.push(itemObj);
-            console.log(`[${counter}] ${ele.body}`);
-            counter++;
+    // search exact matching tags
+    Object.entries(listObj).forEach(([tag, itemObj]) => {
+      if (tag === term.toUpperCase()) {
+        tagFound = true;
+        itemObj.forEach((item) => {
+          if (!item.done) {
+            console.log(`⭕️ ${item.body}`);
+          } else {
+            console.log(`🔴 ${item.body} [complete]`);
+          }
         });
-    }
+      }
+    });
 
-    // case 4
-    editReminder(editChoice: number, newDescription: string) {
-        let r_id = this.itemList[editChoice - 1].r_id;
-        let r_index: number;
+    if (!tagFound) {
+      // if no tag found above, proceed to search body value
+      Object.values(listObj).forEach((itemObj) => {
+        // looping tag level
 
-        this.noteList.forEach(reminder => {
-            if (reminder.r_id === r_id) {
-                r_index = this.noteList.indexOf(reminder);
-                this.noteList[r_index].body = newDescription;
+        itemObj.forEach((item) => {
+          // looping item-obj array
+          if (
+            item.body.toUpperCase() === term.toUpperCase() ||
+            item.body.toUpperCase().includes(term.toUpperCase())
+          ) {
+            if (!item.done) {
+              console.log(`⭕️ ${item.body}\n`);
+            } else {
+              console.log(`🔴 ${item.body} [complete]\n`);
             }
-        })
+          }
+        });
+      });
     }
+  }
 
-    // case 5 
-    toggleComplete(chosen_index: number) {
-        let r_id = this.itemList[chosen_index - 1].r_id;
-        let r_index: number;
+  // case 3
+  addNote(body: string, tag: string) {
+    this.noteList.push(new Reminder(body, tag));
+  }
 
-        this.noteList.forEach( reminder => {
-            if (reminder.r_id === r_id) {
-                r_index = this.noteList.indexOf(reminder);
-                this.noteList[r_index].done = !reminder.done;
-            }
-        })
-        console.log('');
-    }
+  // display edit/toggle list
+  displayList() {
+    let counter: number = 1;
+    this.noteList.forEach((ele) => {
+      let itemObj: ItemObj = { body: ele.body, done: ele.done, r_id: ele.r_id };
+      this.itemList.push(itemObj);
+      console.log(`[${counter}] ${ele.body}`);
+      counter++;
+    });
+  }
 
-};
+  // case 4
+  editReminder(editChoice: number, newDescription: string) {
+    let r_id = this.itemList[editChoice - 1].r_id;
+    let r_index: number;
+
+    this.noteList.forEach((reminder) => {
+      if (reminder.r_id === r_id) {
+        r_index = this.noteList.indexOf(reminder);
+        this.noteList[r_index].body = newDescription;
+      }
+    });
+  }
+
+  // case 5
+  toggleComplete(chosen_index: number) {
+    let r_id = this.itemList[chosen_index - 1].r_id;
+    let r_index: number;
+
+    this.noteList.forEach((reminder) => {
+      if (reminder.r_id === r_id) {
+        r_index = this.noteList.indexOf(reminder);
+        this.noteList[r_index].done = !reminder.done;
+      }
+    });
+    console.log("");
+  }
+}
 
 export { ReminderBook };
-
